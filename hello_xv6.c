2814f128a485d1ee668ef0b5cb377c605ac5f9ab
quakthemaster <vijaywatsanidhya@gmail.com>
2017-04-14 14:52:55 +0530

    Commit after the hello_xv6 change

---
+++ hello_xv6.c
@@ -0,0 +1,8 @@
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void sanidhya(void){
+		printf(1,"Hello_world_I_need_you\n");
+}
---
+++ hello_xv6.d
@@ -0,0 +1,1 @@
+hello_xv6.o: hello_xv6.c /usr/include/stdc-predef.h types.h stat.h user.h
